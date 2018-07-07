2fa5f25a753d7aa07c558eaee11f7b1bc8d8399a
ryokosuge <ryo.kosuge@gmail.com>
2018-07-07 13:47:23 +0900

    fix:    components directory内も分けられるように

--- src/app/entry/component/app.component.vue
+++ src/app/entry/component/app.component.vue
@@ -6,9 +6,8 @@
 </template>
 
 <script>
-// import Home from '../../components/Home.vue';
-import Home from '@/components/Home.vue';
-import Editor from '@/components/Editor.vue';
+import Home from '@/components/pages/Home.vue';
+import Editor from '@/components/pages/Editor.vue';
 
 export default {
   name: 'App',
