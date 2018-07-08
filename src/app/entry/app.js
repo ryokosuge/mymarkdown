8e1d90bd2440b2da31d16960f337492ddd630670
ryokosuge <ryo.kosuge@gmail.com>
2018-07-08 22:16:11 +0900

    feat:   adding vuetify

--- src/app/entry/app.js
+++ src/app/entry/app.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './component/app.component.vue';
+
+Vue.use(Vuetify)
+
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
 export function createApp() {
   const app = new Vue({
